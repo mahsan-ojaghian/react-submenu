a2ee9d78feb2432c0daebf059b127f5887f9c5f0
Mahsan Ojaghian <77487326+mahsan-ojaghian@users.noreply.github.com>
2021-08-31 16:40:37 +0430

    app.js added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import "./styles.css";
-
-export default function App() {
+import React from 'react';
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Sidebar from './Sidebar';
+import Submenu from './Submenu';
+function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
+    </>
   );
 }
+
+export default App;
